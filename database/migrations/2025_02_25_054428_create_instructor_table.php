2ef88c7a3bb128c26390a187a24f0de27fcff0f6
kiannaquines <kjgnaquines@gmail.com>
2025-02-25 13:46:32 +0800

    added instructor model

---
+++ database/migrations/2025_02_25_054428_create_instructor_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('instructor', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('middlename');
+            $table->string('lastname');
+            $table->string('extension')->nullable();
+            $table->string('employee_id');
+            $table->foreignId('department_parent_id')->references('id')->on('department');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('instructor');
+    }
+};
