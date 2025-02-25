44a460e619cc0b391954d85d95cda07e44867e8a
kiannaquines <kjgnaquines@gmail.com>
2025-02-25 12:11:27 +0800

    modeling the database tables

--- database/migrations/2025_02_25_035012_create_student_table.php
+++ database/migrations/2025_02_25_035012_create_student_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('student', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('student_uuid')->primary();
             $table->string('firstname');
             $table->string('lastname');
             $table->string('middlename');
