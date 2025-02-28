4bcd972f4037f035d686feed3066a11d3a90fc2d
kiannaquines <kjgnaquines@gmail.com>
2025-02-28 15:08:06 +0800

    modified the name of the repository

--- app/Domains/Student/Services/EloquentStudentRepository.php
+++ app/Domains/Student/Services/EloquentStudentRepository.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace App\Domains\Student\Repositories;
+namespace App\Domains\Student\Services;
 
 use App\Domains\Student\Entities\Student;
 use App\Domains\Student\Interface\IEloquentStudentRepository;
 
-class EloquentEloquentStudentRepository implements IEloquentStudentRepository {
+class EloquentStudentRepository implements IEloquentStudentRepository {
     public function findById(string $id): ?Student
     {
         return $this->findById($id);
