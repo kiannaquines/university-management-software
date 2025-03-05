254365d6169d3bd7c14a3ab66c9137e36c752d6a
kiannaquines <kjgnaquines@gmail.com>
2025-03-05 23:15:35 +0800

    added files for authentication

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('web')->group(function () {
-    require base_path('app/Application/Routes/HomeRoutes.php');
-    require base_path('app/Application/Routes/AuthRoutes.php');
-    require base_path('app/Application/Routes/CollegeRoutes.php');
-    require base_path('app/Application/Routes/StudentRoutes.php');
+Route::get('/', function () {
+    return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
